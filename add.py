e59905c5bfc96053f504750428c671a64d188605
Stephen Krings <krings.stephen@gmail.com>
2016-08-22 14:48:47 -0700

    .

---
+++ add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+s1 = ['mem_id', 'username', 'fname', 'lname', 'twitter', 'charity', 'charity_dollars', 'picture', 'email', 'pick1', 'pick2', 'pick3', 'pick4', 'challenger1', 'challenger2', 'challenger3', 'challenger4']
+
+s2 = ['$' + s for s in s1]
